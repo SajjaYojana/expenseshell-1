1d50aa69f8cf1011aba5e64ea31478da376e2410
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-23 16:58:28 -0500

    backend

--- Backend.sh
+++ Backend.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 source ./common.sh
+
 check_root
 
 echo "Please enter DB password:"
-read mysql_root_password
+read -s mysql_root_password
 
+dnf module disable nodejs -y &>>$LOGFILE
 dnf module enable nodejs:20 -y &>>$LOGFILE
-
 dnf install nodejs -y &>>$LOGFILE
 
 id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     useradd expense &>>$LOGFILE
-   
 else
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
@@ -30,7 +30,7 @@ unzip /tmp/backend.zip &>>$LOGFILE
 npm install &>>$LOGFILE
 
 #check your repo and path
-cp /home/ec2-user/expenseshell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 systemctl daemon-reload &>>$LOGFILE
 
@@ -40,6 +40,6 @@ systemctl enable backend &>>$LOGFILE
 
 dnf install mysql -y &>>$LOGFILE
 
-mysql -h db.daws78s.fun -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 
 systemctl restart backend &>>$LOGFILE
