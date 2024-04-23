668d77ce58b682fb939bb4735b79da3b29976733
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-23 16:03:44 -0500

    db

--- mysql.sh
+++ mysql.sh
@@ -8,10 +8,14 @@ echo "Please enter DB password:"
 read mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
+
 
 #Below code will be useful for idempotent nature
 mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
